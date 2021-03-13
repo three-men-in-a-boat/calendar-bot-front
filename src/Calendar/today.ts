84f589988a15ecfbbb0ed28b2c7f279019e11606
alexey ershkov <ershkov.alex2000@gmail.com>
2021-03-13 18:52:50 +0300

    events done

--- src/Calendar/today.ts
+++ src/Calendar/today.ts
@@ -1,8 +1,22 @@
 import {Context, Markup, Telegraf} from 'telegraf';
+import {default as axios} from 'axios'
 
 export default function Today(bot: Telegraf<Context>) {
-    function todayCallback(ctx: Context) {
-        const answerStr = 'Ваши встречи на сегодня';
+
+
+    async function todayCallback(ctx: Context) {
+        let answerStr = 'Ваши встречи на сегодня\n';
+
+        const resp = await axios.get('https://calendarbot.xyz/api/v1/oauth/telegram/events')
+        const meetings: Array<Object> = resp.data
+
+
+        meetings.forEach((curr, idx) => {
+            // @ts-ignore
+            let pars = curr.participants[0].split(',').join('\n\t\t\t')
+            // @ts-ignore
+            answerStr += `\n\n${idx + 1}) ${curr.name} ${curr.time} with \n\t\t\t ${pars}`
+        })
 
         return ctx.reply(
             answerStr,
