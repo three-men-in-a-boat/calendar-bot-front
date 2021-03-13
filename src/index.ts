dde554ae823a628efe0d9d4891e8e17412962ad8
alexey ershkov <ershkov.alex2000@gmail.com>
2021-03-13 16:45:33 +0300

    actions started

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Telegraf} from 'telegraf';
+import {Telegraf, Markup} from 'telegraf';
 import {config} from 'dotenv';
 
 config();
@@ -6,25 +6,41 @@ config();
 const token = process.env['TELEGRAM_BOT_TOKEN'];
 
 if (!token) {
-  console.log('.env file not found');
-  throw new Error('Telegram token not found');
+    console.log('.env file not found');
+    throw new Error('Telegram token not found');
 }
 
 const bot = new Telegraf(token);
-bot.start(ctx => ctx.reply('Welcome'));
+bot.start(
+    async ctx =>  {
+
+        ;
+        return ctx.reply('Вы покинули чат')
+
+        // return ctx.reply('Добрый день, пожалуйста авторизуйтесь для начала работы с календарем',
+        //     Markup.inlineKeyboard([
+        //         Markup.button.url('Войти в аккаунт', 'https://oauth.mail.ru/xlogin?' +
+        //             'client_id=885a013d102b40c7a46a994bc49e68f1&' +
+        //             'response_type=code&scope=&' +
+        //             'redirect_uri=https://calendarbot.xyz/api/v1/login&' +
+        //             'state=some_state')
+        //     ]))
+    })
 bot.help(ctx => {
-  ctx.reply('Send me a sticker');
+    ctx.reply('Send me a sticker');
 });
 bot.on('sticker', ctx => ctx.reply('👍'));
 bot.hears('hi', ctx => ctx.reply('Hey there'));
+
+
 bot
-  .launch()
-  .then(() => {
-    console.log('Bot started');
-  })
-  .catch(err => {
-    console.log(`There was an error: ${err}`);
-  });
+    .launch()
+    .then(() => {
+        console.log('Bot started');
+    })
+    .catch(err => {
+        console.log(`There was an error: ${err}`);
+    });
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
