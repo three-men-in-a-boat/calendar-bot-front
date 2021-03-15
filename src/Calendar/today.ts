88a4bfa06efff8d21a2689b236181296952a43da
alexey ershkov <ershkov.alex2000@gmail.com>
2021-03-15 19:22:43 +0300

    Meeting model added and fix linters

--- src/Calendar/today.ts
+++ src/Calendar/today.ts
@@ -1,36 +1,36 @@
 import {Context, Markup, Telegraf} from 'telegraf';
-import {default as axios} from 'axios'
+import {default as axios} from 'axios';
+import Meeting from '../Models/Meeting';
 
 export default function Today(bot: Telegraf<Context>) {
+  async function todayCallback(ctx: Context) {
+    let answerStr = 'Ваши встречи на сегодня\n';
 
+    const resp = await axios.get(
+      'https://calendarbot.xyz/api/v1/oauth/telegram/events'
+    );
+    const meetings: Array<Meeting> = resp.data;
 
-    async function todayCallback(ctx: Context) {
-        let answerStr = 'Ваши встречи на сегодня\n';
-
-        const resp = await axios.get('https://calendarbot.xyz/api/v1/oauth/telegram/events')
-        const meetings: Array<Object> = resp.data
-
-
-        meetings.forEach((curr, idx) => {
-            // @ts-ignore
-            let pars = curr.participants[0].split(',').join('\n\t\t\t')
-            // @ts-ignore
-            answerStr += `\n\n${idx + 1}) ${curr.name} ${curr.time} with \n\t\t\t ${pars}`
-        })
-
-        return ctx.reply(
-            answerStr,
-            Markup.inlineKeyboard([
-                Markup.button.callback('События на завтра', 'tomorrow'),
-            ])
-        );
-    }
-
-    bot.command('today', ctx => {
-        return todayCallback(ctx);
+    meetings.forEach((curr, idx) => {
+      const pars = curr.participants[0].split(',').join('\n\t\t\t');
+      answerStr += `\n\n${idx + 1}) ${curr.name} ${
+        curr.time
+      } with \n\t\t\t ${pars}`;
     });
 
-    bot.action('today', ctx => {
-        return todayCallback(ctx);
-    });
+    return ctx.reply(
+      answerStr,
+      Markup.inlineKeyboard([
+        Markup.button.callback('События на завтра', 'tomorrow'),
+      ])
+    );
+  }
+
+  bot.command('today', ctx => {
+    return todayCallback(ctx);
+  });
+
+  bot.action('today', ctx => {
+    return todayCallback(ctx);
+  });
 }
