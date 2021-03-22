1eaf3d51623946e7c9c610109b4f073e2accf3b7
alexey ershkov <ershkov.alex2000@gmail.com>
2021-03-23 01:23:51 +0300

    update urls and workflows

--- src/Middlewares/auth_middleware.ts
+++ src/Middlewares/auth_middleware.ts
@@ -4,27 +4,24 @@ import {Context} from 'telegraf';
 const whitelist = ['/start'];
 
 export default async function AuthMiddleware(ctx: Context, next: Function) {
-  //TODO - вынести url в константы
-  if (ctx.from && ctx.message) {
-    if (ctx.message) {
-      // Поле text есть - в типах не описано
+    //TODO - вынести url в константы
+    if (ctx.from && ctx.message) {
+        if (ctx.message) {
+            // Поле text есть - в типах не описано
 
-      // @ts-ignore
-      if (whitelist.includes(ctx.message.text)) {
-        return next();
-      }
+            // @ts-ignore
+            if (whitelist.includes(ctx.message.text)) {
+                return next();
+            }
+        }
+        const res = await axios.get(`https://calendarbot.xyz/api/v1/oauth/telegram/user/${ctx.from.id}/is_auth`);
+        if (res.status !== 200) {
+            return ctx.reply(
+                'К сожалению вы не авторизованы. Войдите в систему с помощью команды ' +
+                '/start'
+            );
+        }
     }
-    // const res = await axios.get(`https://calendarbot.xyz/api/v1/oauth/telegram/user/${ctx.from.id}/is_auth`);
-    // if (res.status === 200) {
-    //
-    // } else {
-    return ctx.reply(
-      'К сожалению вы не авторизованы. Войдите в систему с помощью команды ' +
-        '/start'
-    );
-    // }
-    // }
-  }
 
-  return next();
+    return next();
 }
