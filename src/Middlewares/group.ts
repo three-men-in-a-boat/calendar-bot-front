698da3eea80d04a23ccb3c3ee881dfaaa15eec24
alexey ershkov <ershkov.alex2000@gmail.com>
2021-03-31 10:58:22 +0300

    add group middleware

---
+++ src/Middlewares/group.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import CustomContext from "../Models/CustomContext";
+
+const whitelist = ['/today','/start','/start@three_man_in_a_boat_front_bot', '/today@three_man_in_a_boat_front_bot'];
+
+export default async function GroupMiddleware(ctx: CustomContext, next: Function) {
+    if (ctx.from) {
+        if (ctx.message) {
+            // Поле text есть - в типах не описано
+
+            // @ts-ignore
+            if (whitelist.includes(ctx.message.text)) {
+                return next();
+            }
+        }
+        return ctx.reply('Эта функция еще не поддерживается в групповом чате.')
+    } else {
+        return ctx.reply('Group middleware error: ctx.from is undefined');
+    }
+}
