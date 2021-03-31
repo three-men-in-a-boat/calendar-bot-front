5931cea9d811d5ef8bde9fc66cc21c1340719f12
alexey ershkov <ershkov.alex2000@gmail.com>
2021-03-31 15:18:53 +0300

    hotfix

--- src/Middlewares/group.ts
+++ src/Middlewares/group.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import CustomContext from "../Models/CustomContext";
+import getChatType from "../utils/get_chat_type";
 
 const whitelist = ['/today','/start','/start@three_man_in_a_boat_front_bot', '/today@three_man_in_a_boat_front_bot'];
 
@@ -7,15 +8,14 @@ export default async function GroupMiddleware(ctx: CustomContext, next: Function
     if (ctx.from) {
         if (ctx.message) {
             // Поле text есть - в типах не описано
-
-            // @ts-ignore
-            if (whitelist.includes(ctx.message.text)) {
-                return next();
+            if (getChatType(ctx) === 'group') {
+                // @ts-ignore
+                if (whitelist.includes(ctx.message.text)) {
+                    return next();
+                }
+                return ctx.reply('Эта функция еще не поддерживается в групповом чате.')
             }
-            return ctx.reply('Эта функция еще не поддерживается в групповом чате.')
         }
     }
     return next();
-
-
 }
